d73e16545c82dfcf88d41eafe2f1f39e9b980307
mattperkins <github@mattperkins.com>
2018-06-28 16:31:09 +0100

    cl

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const app = express()
 
 // BodyParser Middleware
 app.use(bodyParser.json())
+
+// Db config
+const db = require('./config/keys').mongoURI
+
+// Connect to Mongo
+mongoose.connect(db)
+.then(()=>{console.log('connected')})
+.catch(err => console.log(err))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>console.log(`server running on ${port}`))
